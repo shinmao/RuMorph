d13700a00ef3adeea34624f199df8fcd7077cc2b
shinmao <shinmaox2016@gmail.com>
2023-05-30 19:20:27 +0000

    update crawler to get top 20k downloaded crates

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,87 +1,97 @@
-#![feature(rustc_private)]
+use std::error;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use csv::ReaderBuilder;
+use libflate::gzip::Decoder;
+use serde::de::DeserializeOwned;
+use serde_derive::Deserialize;
+use semver::Version;
+use tar::Archive;
+use rayon::prelude::*;
 
-extern crate rustc_ast_pretty;
-extern crate rustc_driver;
-extern crate rustc_error_codes;
-extern crate rustc_errors;
-extern crate rustc_hash;
-extern crate rustc_hir;
-extern crate rustc_interface;
-extern crate rustc_session;
-extern crate rustc_span;
+const MAX_CRATE_SIZE: usize = 20 * 1000;
 
-use std::{path, process, str};
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
-use rustc_ast_pretty::pprust::item_to_string;
-use rustc_errors::registry;
-use rustc_session::config::{self, CheckCfg};
-use rustc_span::source_map;
-
-fn main() {
-    let out = process::Command::new("rustc")
-        .arg("--print=sysroot")
-        .current_dir(".")
-        .output()
-        .unwrap();
-    let sysroot = str::from_utf8(&out.stdout).unwrap().trim();
-    let config = rustc_interface::Config {
-        opts: config::Options {
-            maybe_sysroot: Some(path::PathBuf::from(sysroot)),
-            ..config::Options::default()
-        },
-        input: config::Input::Str {
-            name: source_map::FileName::Custom("main.rs".to_string()),
-            input: r#"
-fn main() {
-    let message = "Hello, World!";
-    println!("{message}");
+#[derive(Deserialize, Debug)]
+struct Crate {
+    #[serde(rename = "id")]
+    crate_id: u64,
+    name: String,
+    downloads: u64,
+    description: Option<String>,
+    repository: Option<String>,
+    #[serde(skip_deserializing, default = "default_version")]
+    version: Version,
 }
-"#
-            .to_string(),
-        },
-        crate_cfg: rustc_hash::FxHashSet::default(),
-        crate_check_cfg: CheckCfg::default(),
-        output_dir: None,
-        output_file: None,
-        file_loader: None,
-        locale_resources: rustc_driver::DEFAULT_LOCALE_RESOURCES,
-        lint_caps: rustc_hash::FxHashMap::default(),
-        parse_sess_created: None,
-        register_lints: None,
-        override_queries: None,
-        make_codegen_backend: None,
-        registry: registry::Registry::new(&rustc_error_codes::DIAGNOSTICS),
-    };
-    rustc_interface::run_compiler(config, |compiler| {
-        compiler.enter(|queries| {
-            // TODO: add this to -Z unpretty
-            let ast_krate = queries.parse().unwrap().get_mut().clone();
-            for item in ast_krate.items {
-                println!("{}", item_to_string(&item));
-            }
-            // Analyze the crate and inspect the types under the cursor.
-            queries.global_ctxt().unwrap().enter(|tcx| {
-                // Every compilation contains a single crate.
-                let hir_krate = tcx.hir();
-                // Iterate over the top-level items in the crate, looking for the main function.
-                for id in hir_krate.items() {
-                    let item = hir_krate.item(id);
-                    // Use pattern-matching to find a specific node inside the main function.
-                    if let rustc_hir::ItemKind::Fn(_, _, body_id) = item.kind {
-                        let expr = &tcx.hir().body(body_id).value;
-                        if let rustc_hir::ExprKind::Block(block, _) = expr.kind {
-                            if let rustc_hir::StmtKind::Local(local) = block.stmts[0].kind {
-                                if let Some(expr) = local.init {
-                                    let hir_id = expr.hir_id; // hir_id identifies the string "Hello, world!"
-                                    let def_id = item.hir_id().owner.def_id; // def_id identifies the main function
-                                    let ty = tcx.typeck(def_id).node_type(hir_id);
-                                    println!("{expr:#?}: {ty:?}");
-                                }
-                            }
-                        }
-                    }
-                }
-            })
-        });
+
+#[derive(Deserialize, Debug)]
+struct CrateVersion {
+    crate_id: u64,
+    num: Version,
+}
+
+fn default_version() -> Version {
+    Version::parse("0.0.0").unwrap()
+}
+
+fn read_csv<D: DeserializeOwned>(file: impl Read) -> Result<Vec<D>> {
+    let mut records: Vec<D> = vec![];
+    let mut reader = ReaderBuilder::new().has_headers(true).from_reader(file);
+    for record in reader.deserialize() {
+        records.push(record?);
+    }
+    Ok(records)
+}
+
+fn executor() -> Result<()> {
+    let mut crates: Vec<Crate> = Vec::new();
+    let mut versions: Vec<CrateVersion> = Vec::new();
+
+    let csv_path = "../db-dump.tar.gz";
+    let mut archive = Archive::new(
+        Decoder::new(
+            BufReader::new(
+                File::open(csv_path)?
+            )
+        )?
+    );
+    let entries = archive.entries()?.filter(|entry| {
+        // Only filter the file we needed.
+        entry
+            .as_ref()
+            .unwrap()
+            .path()
+            .unwrap()
+            .file_name()
+            .and_then(|f| f.to_str())
+            .map(|f| ["crates.csv", "versions.csv"].contains(&f))
+            .unwrap()
     });
+    for file in entries {
+        let file = file?;
+        println!("{:?}", file.path()?);
+
+        if let Some(filename) = file.path()?.file_name().and_then(|f| f.to_str()) {
+            match filename {
+                "crates.csv" => {
+                    crates = read_csv(file)?;
+                }
+                "versions.csv" => {
+                    versions = read_csv(file)?;
+                }
+                _ => {}
+            }
+        }
+    }
+    crates.par_sort_unstable_by(|a, b| b.downloads.cmp(&a.downloads));
+    crates = crates.into_iter().take(MAX_CRATE_SIZE).collect();
+    for c in crates {
+        println!("{:?}", c);
+    }
+    Ok(())
+}
+
+fn main() {
+    let _ = executor();
 }
