d7b29ec915181082fb9cbae16490e60c8152dce2
shinmao <shinmaox2016@gmail.com>
2023-08-02 03:41:40 +0000

    [Broken Layout Checker] add test case

--- tests/bl1/src/main.rs
+++ tests/bl1/src/main.rs
@@ -1,11 +1,28 @@
-fn risky_function(val: *const i32) {
+fn risky_cast_II(val: *const i32) -> *const i64 {
+    val as *const i64
+}
+
+fn risky_cast(val: &i32) -> &i64 {
+    let t = val as *const i32 as *const i64;
+    unsafe { &*t }
+}
+
+fn risky_transmute_II<'a>(val: &'a i32, val2: &'a i64) -> &'a i64 {
+    let t: &i64 = unsafe { core::mem::transmute(val) };
+    val2
+}
+
+fn risky_transmute(val: *const i32) -> *const i64{
     let t: *const i64 = unsafe {
         std::mem::transmute(val)
     };
-    println!("{}", unsafe { &*t });
+    t
+}
+
+fn safe_transmute(val: &i64) -> &i32 {
+    unsafe { core::mem::transmute(val) }
 }
 
 fn main() {
     let a: i32 = 3;
-    risky_function(&a as *const i32);
 }
